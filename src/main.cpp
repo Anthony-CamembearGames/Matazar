dbd58c7fd495f96e981d00eab8db27b6ed27abaf
Anthony <CamembearGames@proton.me>
2025-02-17 15:42:51 +0100

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,4 @@
-#include <iostream>
-int main() {
-
-
-
-  std::cout << "Hello World!";
-  for (int i = 0; i < 10; i++) {
-    std::cout << i;
-    std::cout << i;
-  }
+int int main (int argc, char *argv[]) {
+  std::
   return 0;
 }
